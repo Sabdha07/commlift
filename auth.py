9a7eaf2bb08f67bac519bcc9d22cd0b367273bb1
Sabdhayini K B <108819283+Sabdha07@users.noreply.github.com>
2025-06-06 04:40:09 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,16 +1,16 @@
 import pyrebase
-import os
+import streamlit as st
 
-# Build firebase_config dict from environment variables
+# Build firebase_config dict from streamlit 
 firebase_config = {
-    "apiKey": os.getenv("FIREBASE_API_KEY"),
-    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
-    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
-    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
-    "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
-    "appId": os.getenv("FIREBASE_APP_ID"),
-    "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID"),
-    "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
+    "apiKey": st.secrets["firebase"]["api_key"],
+    "authDomain": st.secrets["firebase"]["auth_domain"],
+    "projectId": st.secrets["firebase"]["project_id"],
+    "storageBucket": st.secrets["firebase"]["storage_bucket"],
+    "messagingSenderId": st.secrets["firebase"]["messaging_sender_id"],
+    "appId": st.secrets["firebase"]["app_id"],
+    "measurementId": st.secrets["firebase"]["measurement_id"],
+    "databaseURL": st.secrets["firebase"]["database_url"],
 }
 
 firebase = pyrebase.initialize_app(firebase_config)
